e33d618b48c3a6b557761f6bac7c06b8d6c9f86b
Admin <denysenko.artem1624@vu.cdu.edu.ua>
2026-06-02 16:02:43 +0300

    Add state management and localStorage persistence

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log("JS connected!");
+
 const themeToggleBtn = document.getElementById('theme-toggle');
 const body = document.body;
 
@@ -62,11 +63,8 @@ async function loadPosts() {
         }
 
         const data = await response.json();
-        
         allPosts = data.slice(0, 10);
-        
         renderPosts(allPosts);
-
         loading.style.display = 'none';
 
     } catch (error) {
@@ -99,11 +97,74 @@ const searchInput = document.querySelector('#search-input');
 if (searchInput) {
     searchInput.addEventListener('input', () => {
         const value = searchInput.value.toLowerCase();
-
         const filtered = allPosts.filter(post =>
             post.title.toLowerCase().includes(value)
         );
-
         renderPosts(filtered);
     });
 }
+
+
+let tasks = [];
+
+const taskInput = document.querySelector('#task-input');
+const addTaskBtn = document.querySelector('#add-task');
+const taskList = document.querySelector('#task-list');
+
+function saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function loadTasks() {
+    const data = localStorage.getItem('tasks');
+    if (data) {
+        tasks = JSON.parse(data);
+    }
+}
+
+function renderTasks() {
+    if (!taskList) return;
+    taskList.innerHTML = '';
+
+    tasks.forEach((task, index) => {
+        const li = document.createElement('li');
+        li.textContent = task.text;
+
+        const btn = document.createElement('button');
+        btn.textContent = 'X';
+        btn.classList.add('delete-task-btn');
+
+        btn.addEventListener('click', () => {
+            tasks.splice(index, 1);
+            saveTasks();
+            renderTasks();
+        });
+
+        li.appendChild(btn);
+        taskList.appendChild(li);
+    });
+}
+
+if (addTaskBtn && taskInput) {
+    addTaskBtn.addEventListener('click', () => {
+        const value = taskInput.value.trim();
+
+        if (value === '') return;
+
+        tasks.push({ text: value });
+        saveTasks();
+        renderTasks();
+
+        taskInput.value = '';
+    });
+
+
+    taskInput.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            addTaskBtn.click();
+        }
+    });
+}
+
+loadTasks();
+renderTasks();
